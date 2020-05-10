d8b2754c817ba1b6d5cbab87240b7bdfe38d34a7
Renan Riso <renriso@gmail.com>
2020-05-10 20:50:18 -0300

    Initial commit

---
+++ Assets/Script/JewelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JewelController : MonoBehaviour {
+
+    /*Verifica as colisões da pedra*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player") //se quem colidiu com a pedra é o jogador, então troca a pedra de posição.
+        {
+            if (transform.position.x == -5)
+            {
+                transform.position = new Vector3(5, -3f);
+            }
+            else
+            {
+                transform.position = new Vector3(-5, -3f);
+            }
+            
+        }
+    }
+}
